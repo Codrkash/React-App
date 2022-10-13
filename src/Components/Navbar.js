b7193ec66b01b0aecbd41682751ab0ecc0e49657
Sodium Sharma <sodiumsharmaji@gmail.com>
2022-10-13 19:53:51 +0530

    UI Updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/"></a>
           <button
             className="navbar-toggler"
             type="button"
